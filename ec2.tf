ef82b68b45a8b154fe44dbbab1e9129210c1f6b3
Ryan Archibald <ryanarchibald9219@gmail.com>
2022-09-02 12:32:38 +0100

    adding count to instance type

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ec2-vm" {
   ami                    = data.aws_ami.amzlinux.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  count                  = 1
   tags = {
     "Name" = "Terraform-Cloud"
   }
